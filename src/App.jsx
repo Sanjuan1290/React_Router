df9ab5d139fc1b71474cc0583a538cdd72c1b887
robert <robertrenbysanjuan@gmail.com>
2025-05-31 10:36:36 +0800

    add: fake server.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './components/Footer'
 import Home from './components/Home'
 import About from './components/About'
 
+import "./server"
+
 export default function App(){
 
     return(
