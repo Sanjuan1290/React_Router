2582c50e6e8d4941662354e1ad212ac8d7e6bc5c
robert <robertrenbysanjuan@gmail.com>
2025-05-31 10:16:37 +0800

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+
+export default function Home(){
+
+    return(
+        <section className="homePage">
+            <img src="/homeImage.png" alt="home page Image" />
+
+            <section className="content-container">
+                <h1>You got the travel plans, we got the travel vans.</h1>
+                <small>
+                    Add adventure to your life by joining the #vanlife movement.
+                    Rent the perfect van to make your perfect road trip.
+                </small>
+
+                <button>Find your van</button>
+            </section>
+        </section>
+    )
+}
