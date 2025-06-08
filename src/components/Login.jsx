38bb507d871b7218e851ad9f4f07a9451ed83e9c
robert <robertrenbysanjuan@gmail.com>
2025-06-08 09:51:39 +0800

    refactor: get url message and display

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,13 @@
+import { useLoaderData } from "react-router-dom"
+
+export function loader(obj){
+    const message = new URL(obj.request.url).searchParams.get('message')
+    return message
+}
 
 export default function Login(){
+
+    const message = useLoaderData()
 
     function handleSubmitForm(event){
         event.preventDefault()
@@ -15,6 +23,7 @@ export default function Login(){
         <>
             <form onSubmit={handleSubmitForm} className="login">
                 <h1>Sign in to your account</h1>
+                <h2>{message}</h2>
 
                 <input name="email" type="email" placeholder="example@gmail.com" defaultValue="robertSanJuan@gmail.com"/>
                 <input name="password" type="password" placeholder="password" defaultValue="1234567890"/>
